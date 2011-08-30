d3ac6d6d5bcbd60cf780ab6eb852c34dbcec8554
Jonanin <jonanin@gmail.com>
2011-08-30 00:06:27 -0500

    finish LFS test code.

--- lfstest.c
+++ lfstest.c
@@ -4,18 +4,31 @@
 #include "fs.h"
 #include "fcntl.h"
 
+#define NFILE 40
+#define BUFLEN 20000
+
 int
 main(int argc, char *argv[])
 {
-	int fd = open("lfstest0", O_CREATE | O_RDWR);
+	int st = uptime(), k = 0, i = 0;
 
-	int st = uptime(), i = 0;
-	for (i = 0; i < 100; i++)
-		printf(fd, "0");
+	char * buf = malloc(BUFLEN);
+
+	for (k = 0; k < NFILE; k++) {
+		printf(1, "[%d%%] file %d\n", (int)((float)k / NFILE * 100), k);
+		char path[] = "lfstest0";
+		path[7] += k;
+		int fd = open(path, O_CREATE | O_RDWR);
+		for (i = 0; i < (20000 / BUFLEN); i++)
+			if (write(fd, buf, BUFLEN) != BUFLEN) {
+				printf(1, "fail");
+				exit();
+			}
+		close(fd);
+	}
+
 	int en = uptime();
 
-	close(fd);
 	printf(1, "time: %d\n", en - st);
-
 	exit();
 }
